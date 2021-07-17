16f64cddc34bc170452c4cb8010defae2bcb988e
David Mwendwa <klatzdavie19@gmail.com>
2021-07-17 19:19:15 +0300

    feat: write form data to a text file

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+from os import write
 from flask import Flask, render_template, url_for, request, redirect
 
 app = Flask(__name__)
@@ -12,11 +13,19 @@ def home():
 def html_page(page_name):
   return render_template(page_name)
 
+def write_to_file(data):
+  with open('database.txt', mode='a') as database:
+    print(database)
+    email = data["email"]
+    subject = data["subject"]
+    message = data["message"]
+    file = database.write(f'\n{email},{subject},{message}')
+
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
   if request.method == 'POST':
     data = request.form.to_dict()
-    print(data)
+    write_to_file(data)
     return redirect('/thankyou.html')
   else:
     return 'something went wrong'
