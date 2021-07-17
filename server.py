3a40aa2e6b714ddfe641ede81611d896efa71bec
David Mwendwa <klatzdavie19@gmail.com>
2021-07-17 18:45:20 +0300

    configure form for post request and redirect on submit

--- server.py
+++ server.py
@@ -1,11 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request, redirect
 
 app = Flask(__name__)
 print(app)
 print(__name__)
 
+@app.route("/")
+def home():
+  return render_template('index.html')
+
 @app.route("/<string:page_name>")
 def html_page(page_name):
   return render_template(page_name)
 
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+  if request.method == 'POST':
+    data = request.form.to_dict()
+    print(data)
+    return redirect('/thankyou.html')
+  else:
+    return 'something went wrong'
 
